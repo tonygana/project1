9a6472ab411f56c46de33b841506d7e157cf66d8
shengzhiluo000 <65048904+shengzhiluo000@users.noreply.github.com>
2023-11-30 23:46:27 -0500

    Solved research question 1

--- API_keys.py
+++ API_keys.py
@@ -1,8 +1,8 @@
 # Geoapify API Key
-geoapify_key = "key_here"
+geoapify_key = "f0a102120008420a8ebeed9692d9ffb6"
 
 # Yelp API Key
-yelp_key = "key_here"
+yelp_key = "oE4YtQSUak1Arq6h91gfkZvPm2X4EYS2zzlDQUV2U9H7y5Xq3RyRIHv8TwqVIutaqVynOSFya69bCjVqtwqotLMxpa6UgWrIgewRG0mdtQE3ZzJjVxpHm_85IfxnZXYx"
 
 # Google API Key
 google_key = "key_here"
