9672d64d5f7b65b80c6b164cfca5cd7dd1fbdd92
melisatahiraj <melisatahiraj00@gmail.com>
2023-12-05 20:01:06 -0800

    updated version:

--- API_keys.py
+++ API_keys.py
@@ -3,6 +3,3 @@ geoapify_key = "f0a102120008420a8ebeed9692d9ffb6"
 
 # Yelp API Key
 yelp_key = "oE4YtQSUak1Arq6h91gfkZvPm2X4EYS2zzlDQUV2U9H7y5Xq3RyRIHv8TwqVIutaqVynOSFya69bCjVqtwqotLMxpa6UgWrIgewRG0mdtQE3ZzJjVxpHm_85IfxnZXYx"
-
-# Google API Key
-google_key = "AIzaSyBImmDQMz0iud-IM1NCD9kIld6DfNHkVfw"
